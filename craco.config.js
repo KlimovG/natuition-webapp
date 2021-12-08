642e86f08f603b7708f848d437f172f2a2b42cd0
George <klimovgg@gmail.com>
2021-12-08 19:38:38 +0100

    change craco config for mapbox

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,10 @@
 // craco.config.js
 module.exports = {
+  babel: {
+    loaderOptions: {
+      ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
+    },
+  },
   style: {
     postcss: {
       plugins: [
--- tailwind.config.js
+++ tailwind.config.js
@@ -42,4 +42,7 @@ module.exports = {
     },
   },
   plugins: [],
+  eslint: {
+    enable: null,
+  },
 };
